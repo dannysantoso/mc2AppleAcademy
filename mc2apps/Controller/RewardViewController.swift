38a54702d32a1577c62b146c1bdbde97268c18a1
Agnes Felicia <nicoleagnesfelicia@gmail.com>
2020-05-26 23:09:52 +0700

    set up file + xib for reward (nothing inside yet)

---
+++ mc2apps/Controller/RewardViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RewardViewController.swift
+//  mc2apps
+//
+//  Created by Agnes Felicia on 26/05/20.
+//  Copyright © 2020 danny santoso. All rights reserved.
+//
+
+import UIKit
+
+class RewardViewController: UIViewController {
+
+    @IBOutlet weak var rewardImage: UIImageView!
+    @IBOutlet weak var wantButton: UIButton! {
+        didSet {
+            wantButton.layer.cornerRadius = 27.5
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
