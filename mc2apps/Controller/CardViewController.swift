9bb77ee0929ff7e8dd9aa30028ce29094d7da471
melinadewi <hds.melina@gmail.com>
2020-05-26 17:32:49 +0700

    init cardview

---
+++ mc2apps/Controller/CardViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CardViewController.swift
+//  mc2apps
+//
+//  Created by Melina Dewi on 26/05/20.
+//  Copyright © 2020 danny santoso. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(cardTapped))
+//        back.isUserInteractionEnabled = true
+//        back.addGestureRecognizer(tap)
+    }
+    
+//    @objc func cardTapped() {
+//        delegate.cardTapped(self)
+//    }
+//
+//    func cardTapped(_ tapped: CardViewController) {
+//        guard view.isUserInteractionEnabled == true else { return }
+//        view.isUserInteractionEnabled = false
+//
+//        for card in allCards {
+//            if card == tapped {
+//                card.wasTapped()
+//                card.perform(#selector(card.wasntTapped), with: nil, afterDelay: 1)
+//            } else {
+//                card.wasntTapped()
+//            }
+//        }
+//
+//        perform(#selector(loadCards), with: nil, afterDelay: 2)
+//    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
